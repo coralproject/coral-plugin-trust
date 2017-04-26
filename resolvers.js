65b385401ddf42d6c94d021df40b35536a82338d
Wyatt Johnson <wyattjoh@gmail.com>
2017-04-26 08:57:07 -0700

    Cleaned up resolvers

--- resolvers.js
+++ resolvers.js
@@ -3,37 +3,47 @@ const debug = require('debug')('coral-plugin-trust');
 const RELIABLE_THRESHOLD = parseInt(process.env.TRUST_RELIABLE_THRESHOLD || 0);
 const UNRELIABLE_THRESHOLD = parseInt(process.env.TRUST_UNRELIABLE_THRESHOLD || 0);
 
-debug('THRESHOLDS', RELIABLE_THRESHOLD, UNRELIABLE_THRESHOLD);
+debug(`THRESHOLDS Reliable[${RELIABLE_THRESHOLD}] Unreliable[${UNRELIABLE_THRESHOLD}]`);
+
+/**
+ *
+ * determineReliability will inspect the property of the user to match their
+ * reliability score to that of settings.
+ *
+ * @param {Object} property the property containing the karma field to check against
+ */
+const determineReliability = (property) => {
+  if (property.karma > RELIABLE_THRESHOLD) {
+    return true;
+  } else if (property.karma < UNRELIABLE_THRESHOLD) {
+    return false;
+  }
+  return null;
+};
 
 module.exports = {
   User: {
 
-    // Extract the reliability from the user metadata.
-    reliable(user) {
-      if (user && user.metadata && user.metadata.trust) {
-        return user.metadata.trust;
+    // Extract the reliability from the user metadata if they have permission.
+    reliable(user, _, {user: requestingUser}) {
+      if (requestingUser && requestingUser.hasRoles('ADMIN')) {
+        if (user && user.metadata && user.metadata.trust) {
+          return user.metadata.trust;
+        } else {
+          return {};
+        }
       }
-
-      return {};
     }
   },
   Reliability: {
     flagger(trust) {
       if (trust && trust.flag) {
-        if (trust.flag.karma >= RELIABLE_THRESHOLD) {
-          return true;
-        } else if (trust.flag.karma <= UNRELIABLE_THRESHOLD) {
-          return false;
-        }
+        return determineReliability(trust.flag);
       }
     },
     commenter(trust) {
       if (trust && trust.comment) {
-        if (trust.comment.karma >= RELIABLE_THRESHOLD) {
-          return true;
-        } else if (trust.comment.karma <= UNRELIABLE_THRESHOLD) {
-          return false;
-        }
+        return determineReliability(trust.comment);
       }
     }
   }
